f59eedfb61273b8bac02c7c5daacb3433e9189c3
José Pedretti Neto <jpedretti@gmail.com>
2016-11-04 09:12:52 +0000

    added splash screen and ViewPager back navigation with back button

--- app/src/main/java/com/example/jpedretti/masterindandroidapp/MainActivity.java
+++ app/src/main/java/com/example/jpedretti/masterindandroidapp/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.jpedretti.masterindandroidapp;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 
 import com.example.jpedretti.masterindandroidapp.adapter.MyPageAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mPager;
 
     @Override
     protected void onStart() {
@@ -21,12 +22,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MyPageAdapter adapter = new MyPageAdapter(getSupportFragmentManager());
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(adapter);
     }
 
-    @Override
+    /*@Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
+                //getSupportFragmentManager().popBackStack();
+                //return true;
+            } else if (mPager.getCurrentItem() > 0) {
+                mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+                return true;
+            }
+        }
+
         return super.onKeyDown(keyCode, event);
+    }*/
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() > 0) {
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
